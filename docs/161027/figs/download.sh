2ee759e8fe7b240c4b81060b3834369ee7e7ac4f
dabercro <dabercro@mit.edu>
2016-10-27 12:36:55 +0200

    Quick presentation.

---
+++ docs/161027/figs/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+crombie downloadtar "http://dabercro.web.cern.ch/dabercro/plotviewer/viewplot/static/viewplot/returntar.php?only=pdf&files=161026/photon_full_fatjet1tau21,161026/photon_nocut_fatjet1tau21,161026/photon_full_fatjet1PrunedML2L3,161026/photon_nocut_fatjet1PrunedML2L3,161026/photon_full_recoil,161026/photon_nocut_recoil"
+crombie downloadtar "http://dabercro.web.cern.ch/dabercro/plotviewer/viewplot/static/viewplot/returntar.php?only=pdf&files=161026/photon_full_fatjet1Pt,161026/photon_nocut_fatjet1Pt"
+
+for f in */*.pdf
+do
+    pdfcrop $f $f
+done
