49695e684c1296a17f2742e3bfc467d913c66b7c
dabercro <dabercro@mit.edu>
2016-10-28 12:45:00 +0200

    Added log plots quickly.

--- docs/161028/figs/download.sh
+++ docs/161028/figs/download.sh
@@ -3,6 +3,7 @@
 crombie downloadtar "http://dabercro.web.cern.ch/dabercro/plotviewer/viewplot/static/viewplot/returntar.php?only=pdf&files=161026/photon_full_fatjet1tau21,161026/photon_nocut_fatjet1tau21,161026/photon_full_fatjet1PrunedML2L3,161026/photon_nocut_fatjet1PrunedML2L3,161026/photon_full_recoil,161026/photon_nocut_recoil"
 crombie downloadtar "http://dabercro.web.cern.ch/dabercro/plotviewer/viewplot/static/viewplot/returntar.php?only=pdf&files=161026/photon_full_fatjet1Pt,161026/photon_nocut_fatjet1Pt"
 crombie downloadtar "http://dabercro.web.cern.ch/dabercro/plotviewer/viewplot/static/viewplot/returntar.php?only=pdf&files=161027/photon_full_fatjet1tau21,161027/photon_nocut_fatjet1tau21,161027/photon_full_fatjet1PrunedML2L3,161027/photon_nocut_fatjet1PrunedML2L3,161027/photon_full_recoil,161027/photon_nocut_recoil,161027/photon_full_fatjet1Pt,161027/photon_nocut_fatjet1Pt"
+crombie downloadtar "http://dabercro.web.cern.ch/dabercro/plotviewer/viewplot/static/viewplot/returntar.php?only=pdf&files=161028/photon_full_fatjet1tau21,161028/photon_nocut_fatjet1tau21,161028/photon_full_fatjet1PrunedML2L3,161028/photon_nocut_fatjet1PrunedML2L3,161028/photon_full_recoil,161028/photon_nocut_recoil"
 
 for f in */*.pdf
 do
