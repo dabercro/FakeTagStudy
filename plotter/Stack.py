93b48a01ec271067852f293cdc77d3cd1b418fef
Daniel Abercrombie <dabercro@mit.edu>
2016-10-28 12:45:25 +0200

    Should leave Purity.

--- plotter/Stack.py
+++ plotter/Stack.py
@@ -11,7 +11,7 @@ plotter.SetTreeName('events')
 plotter.SetAllHistName('htotal')
 plotter.SetLegendLocation(plotter.kUpper,plotter.kRight,0.25,0.5)
 plotter.SetEventsPer(1.0)
-plotter.SetMinLegendFrac(0.01)
+plotter.SetMinLegendFrac(0.0)
 plotter.SetIgnoreInLinear(0.005)
 plotter.SetOthersColor(922)
 plotter.SetFontSize(0.03)
@@ -29,15 +29,15 @@ def SetupArgs():
         ['fatjet1PtSmearedUp',25,100,600,'p_{T} [GeV]','Events/1.0'],
         ['fatjet1PtSmearedDown',25,100,600,'p_{T} [GeV]','Events/1.0'],
         ['fatjet1Mass',25,0,250,'Fat Jet Mass [GeV]','Events/1.0'],
-        ['fatjet1PrunedML2L3',25,0,250,'Fat Jet Pruned Mass [GeV]','Events/1.0'],
+        ['fatjet1PrunedML2L3',25,0,250,'Fat Jet Pruned Mass [GeV]','Events/1.0', True],
         [{'data_expr':'fatjet1PrunedML2L3'},'fatjet1PrunedML2L3SmearedCentral',25,0,250,'Fat Jet Pruned Mass [GeV]','Events/1.0'],
         [{'data_expr':'fatjet1PrunedML2L3'},'fatjet1PrunedML2L3SmearedUp',25,0,250,'Fat Jet Pruned Mass [GeV]','Events/1.0'],
         [{'data_expr':'fatjet1PrunedML2L3'},'fatjet1PrunedML2L3SmearedDown',25,0,250,'Fat Jet Pruned Mass [GeV]','Events/1.0'],
-        ['fatjet1tau21',25,0,1.5,'#tau_{2}/#tau_{1}','Events/1.0'],
+        ['fatjet1tau21',25,0,1.5,'#tau_{2}/#tau_{1}','Events/1.0', True],
         ['dilepMass',30,0,150,'m_{ll} [GeV]','Events/1.0'],
         ['dilepPt',25,100,600,'p_{T,ll} [GeV]','Events/1.0'],
         ['pho1Pt',25,100,600,'p_{T,ll} [GeV]','Events/1.0'],
-        ['recoil',20,200,1000,'Recoil [GeV]','Events/1.0'],
+        ['recoil',20,200,1000,'Recoil [GeV]','Events/1.0', True],
         ['fatjet1DRGenW',25,0,5,'#Delta R(j,W)','Events/1.0'],
         ]
 
